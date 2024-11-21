95bcf36cc2ae2cd7ff3e04e37298b748afbb31f6
Zeustk <112997729+Zeustk@users.noreply.github.com>
2024-11-20 19:15:33 -0500

    Actualizacion a heroku

--- db.js
+++ db.js
@@ -1,14 +1,28 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize({
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,  // Requiere SSL
+      rejectUnauthorized: false // Desactiva la verificación del certificado (esto es necesario en Render)
+    }
+  }
+});
+
+//const sequelize = new Sequelize({
   database: process.env.PGDATABASE,
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   dialect: 'postgres',
-});
+//});
+
+// Usamos DATABASE_URL si está configurada en Heroku
+
 
 async function Open(sql, params) {
   try {
