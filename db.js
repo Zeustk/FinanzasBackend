299855478fa5100faadbd34b370bdf5748e3ff4e
Zeustk <112997729+Zeustk@users.noreply.github.com>
2024-11-21 00:42:50 -0500

    Pequeños cambios

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ require('dotenv').config();
   }
 });*/
 
-/*const sequelize = new Sequelize({
+const sequelize = new Sequelize({
   database: process.env.PGDATABASE,
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
@@ -26,9 +26,9 @@ require('dotenv').config();
     }
   }
 });
-*/
 
-const sequelize = new Sequelize({
+
+/*const sequelize = new Sequelize({
   database: process.env.PGDATABASE,
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
@@ -36,7 +36,7 @@ const sequelize = new Sequelize({
   port: process.env.PGPORT,
   dialect: 'postgres',
   
-});
+});*/
 
 
 // Usamos DATABASE_URL si está configurada en Heroku
